9d8799fa079aee342cc3a8e36801da5fec83a1a0
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-03-24 10:00:25 -0400

    Updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
@@ -2,28 +2,24 @@ package org.firstinspires.ftc.teamcode.Framework;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class BaseOpMode extends LinearOpMode {
         protected DcMotor[] motors;
-
+        protected Servo claw;
         protected void initHardware() {
-
         motors = new DcMotor[]{
-
                 hardwareMap.dcMotor.get("motor fr"),
                 hardwareMap.dcMotor.get("motor br"),
                 hardwareMap.dcMotor.get("motor fl"),
                 hardwareMap.dcMotor.get("motor bl")
         };
-
         motors[0].setDirection(DcMotorSimple.Direction.REVERSE);
         motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
         motors[2].setDirection(DcMotorSimple.Direction.FORWARD);
         motors[3].setDirection(DcMotorSimple.Direction.FORWARD);
+
+        claw = hardwareMap.get(Servo.class, "servo");
     }
-
-
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
@@ -1,16 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Framework.BaseOpMode;
-
+import org.firstinspires.ftc.teamcode.Framework.Claws;
 
 @TeleOp(name = "Power Play JellyTele")
-
 public class JellyTele extends BaseOpMode {
-
     protected enum DriveMode {
         TANK,
         DRIVE,
@@ -18,36 +18,45 @@ public class JellyTele extends BaseOpMode {
         FIELDCENTRIC,
     }
     protected DriveMode driveMode = DriveMode.FIELDCENTRIC;
-
     public void runOpMode() throws InterruptedException {
-
-        initHardware();
         // Retrieve the IMU from the hardware map
-        imu = hardwareMap.get(IMU.class, "imu");
+        IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-            RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
-            RevHubOrientationOnRobot.UsbFacingDirection.UP));
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
+        initHardware();
         waitForStart();
 
+        while (opModeIsActive()) {
             // DRIVETRAIN
             if (gamepad1.dpad_left) {
                 driveMode = DriveMode.TANK;
+                gamepad1.rumbleBlips(1);
             } else if (gamepad1.dpad_up) {
                 driveMode = DriveMode.MECANUM;
+                gamepad1.rumbleBlips(2);
             } else if (gamepad1.dpad_right) {
                 driveMode = DriveMode.DRIVE;
+                gamepad1.rumbleBlips(3);
             }
             else if (gamepad1.dpad_down) {
                 driveMode = DriveMode.FIELDCENTRIC;
+                gamepad1.rumbleBlips(4);
             }
-            // precision
+            // PRECISION
             double mult = gamepad1.left_bumper ? 0.35 : gamepad1.right_bumper ? 0.7 : 1.0;
-            // IMU Reset
-            if (gamepad1.options) {
+            if (gamepad1.left_bumper){
+                gamepad1.rumble( 1, 1, 10);
+            }
+            if (gamepad1.right_bumper){
+                gamepad1.rumble(2.7,2.7,10);
+            }
+            if (gamepad1.y && gamepad1.back){
                 imu.resetYaw();
+                gamepad1.rumbleBlips(5);
             }
             telemetry.addData("drive mode", driveMode);
             telemetry.addData("mX", gamepad2.left_stick_x);
@@ -72,7 +81,6 @@ public class JellyTele extends BaseOpMode {
                     break;
                 }
                 case MECANUM: {
-                    // right = +, left = -
                     double pivot = gamepad1.right_stick_x;
                     double mX, mY;
                     mX = gamepad1.left_stick_x;
@@ -85,8 +93,8 @@ public class JellyTele extends BaseOpMode {
                     break;
                 }
                 case FIELDCENTRIC: {
-                    double y = -gamepad1.left_stick_y; // Remember, this is reversed
-                    double x = gamepad1.left_stick_x;
+                    double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+                    double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
                     double rx = gamepad1.right_stick_x;
 
                     double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
@@ -102,25 +110,31 @@ public class JellyTele extends BaseOpMode {
                     break;
                 }
             }
+            if(gamepad2.right_bumper){
+                Claws.clawsClose();
+            }
+            if(gamepad2.left_bumper){
+                Claws.clawsOpen();
+            }
+        }
+    }
+    // CLAW
 
-    // precision method
+    // PRECISION METHOD
     protected void setMotorSpeeds(double mult, double[] powers) {
         for (int i = 0; i < 4; i++) {
             powers[i] = powers[i] * mult;
         }
-
         double max = Math.max(Math.max(Math.abs(powers[0]), Math.abs(powers[1])),
                 Math.max(Math.abs(powers[2]), Math.abs(powers[3])));
         double scale = Math.abs(1 / max);
-        // don't increase power, only decrease
+        // SCALES POWER TO KEEP IN RANGE LIMIT
         if (scale > 1) {
             scale = 1;
         }
-
         for (int i = 0; i < 4; i++) {
             powers[i] *= scale;
         }
-
         for (int i = 0; i < 4; i++) {
             motors[i].setPower(powers[i]);
         }
